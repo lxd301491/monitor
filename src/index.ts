b027a99870e21294d30587482fc9bb4166f8ff3d
刘晓东 <lxd301491@gmail.com>
2019-07-14 16:24:34 +0800

    暴露全局枚举

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { MonitorCenter } from "./process/MonitorCenter";
 import { CONSUMER_TYPE } from "./consumers/index";
 import { HOOK_TYPE } from "./hooks/index";
+import * as GlobalEnum from "./configs/globalEnum";
 
 export default {
   MonitorCenter,
   CONSUMER_TYPE,
-  HOOK_TYPE
+  HOOK_TYPE,
+  GlobalEnum
 };
